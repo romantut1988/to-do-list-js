94776370cf9cecabcc9e53a3824c92fe8f52d111
Roman <romantut1988@mail.ru>
2022-01-05 23:29:13 +0300

    todolist

---
+++ script.js
@@ -0,0 +1,74 @@
+const container = document.querySelector('.container');
+const inputValue = document.querySelector('.input');
+const add = document.querySelector('.add');
+
+
+
+class item{
+    constructor(name){
+        this.createItem(name);
+    }
+    createItem(name){
+        const itemBox = document.createElement('div');
+        itemBox.classList.add('item');
+
+        const input = document.createElement('input');
+        input.type = "text";
+        input.disabled = true;
+        input.value = name;
+        input.classList.add('item_input');
+
+        const edit = document.createElement('button');
+        edit.classList.add('edit');
+        edit.innerHTML = "EDIT";
+        edit.addEventListener('click', () => this.edit(input, name));
+
+        const remove = document.createElement('button');
+        remove.classList.add('remove');
+        remove.innerHTML = "REMOVE";
+        remove.addEventListener('click', () => this.remove(itemBox, name));
+
+        container.appendChild(itemBox);
+
+        itemBox.appendChild(input);
+        itemBox.appendChild(edit);
+        itemBox.appendChild(remove);
+
+    }
+
+    edit(input, name){
+        if(input.disabled == true){
+            input.disabled = !input.disabled;
+        }
+        else{
+            input.disabled = !input.disabled;
+            let indexof = todos.indexOf(name);
+            todos[indexof] = input.value;
+            window.localStorage.setItem("todos", JSON.stringify(todos));
+        }
+    }
+
+    remove(itemBox, name){
+        itemBox.parentNode.removeChild(itemBox);
+        let index = todos.indexOf(name);
+        todos.splice(index, 1);
+        window.localStorage.setItem("todos", JSON.stringify(todos));
+    }
+}
+
+add.addEventListener('click', check);
+window.addEventListener('keydown', (e) => {
+    if(e.which == 13){
+        check();
+    }
+})
+
+function check(){
+    if(inputValue.value != ""){
+        new item(inputValue.value);
+        todos.push(inputValue.value);
+        window.localStorage.setItem("todos", JSON.stringify(todos));
+        inputValue.value = "";
+    }
+}
+
